adc7e982555b5a81917325cbeab3f382bac4d4b6
OmarMohamed95 <omar.mo9516@gmail.com>
2020-05-17 13:12:41 +0200

    Check if setting is not empty

--- resources/views/app/inc/footer.blade.php
+++ resources/views/app/inc/footer.blade.php
@@ -1,6 +1,8 @@
 <div class="footer">
     <p class="col-md-offset-5">(c) Developed by OMAR MOHAMED</p>
-    <a href="{{ $sitting->fb }}" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
-    <a href="{{ $sitting->tw }}" target="_blank"><i class="fab fa-twitter-square fa-2x"></i></a>
-    <a href="{{ $sitting->yt }}" target="_blank"><i class="fab fa-youtube-square fa-2x"></i></a>
+    @if ($sitting)
+        <a href="{{ $sitting->fb }}" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+        <a href="{{ $sitting->tw }}" target="_blank"><i class="fab fa-twitter-square fa-2x"></i></a>
+        <a href="{{ $sitting->yt }}" target="_blank"><i class="fab fa-youtube-square fa-2x"></i></a>
+    @endif
 </div>

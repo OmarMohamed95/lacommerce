9a6d30070b18c699661a79e2660b541656d46cf6
OmarMohamed95 <omar.mo9516@gmail.com>
2020-10-24 14:44:30 +0200

    Create cart request

---
+++ app/Http/Requests/App/cartRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\App;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\adminModel\product;
+
+class cartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $product = product::where('id', $this->productId)->first();
+
+        return [
+            'quantity' => "required|Numeric|min:1|max:$product->quantity",
+        ];
+    }
+}

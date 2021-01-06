8cfc1eeeab1efb3833fcd1a08b3f8d4eb7ea29bc
OmarMohamed95 <omar.mo9516@gmail.com>
2021-01-06 10:15:17 +0200

    Fix issue

--- public/js/app.js
+++ public/js/app.js
@@ -45411,8 +45411,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\ecommerce\resources\assets\js\app.js */"./resources/assets/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\ecommerce\resources\assets\sass\app.scss */"./resources/assets/sass/app.scss");
+__webpack_require__(/*! /var/www/html/lacommerce/resources/assets/js/app.js */"./resources/assets/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/lacommerce/resources/assets/sass/app.scss */"./resources/assets/sass/app.scss");
 
 
 /***/ })
